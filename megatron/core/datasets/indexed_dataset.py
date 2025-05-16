511e8f5cbe3ab8291953ac64e5beceb727a1b814
Joel Lamy-Poirier <joel.lamy-poirier@servicenow.com>
2025-05-16 13:17:43 -0400

    version

--- megatron/core/datasets/indexed_dataset.py
+++ megatron/core/datasets/indexed_dataset.py
@@ -228,11 +228,12 @@ class _IndexReader(object):
             assert header == _INDEX_HEADER, f"bad header, cannot read: {idx_path}"
 
             version = struct.unpack("<Q", stream.read(8))[0]
-            assert version in (1, 2), f"bad version, cannot read: {idx_path}"
+            assert version in (1, 2, 3), f"bad version, cannot read: {idx_path}"
 
-            if version == 2:
+            if version >= 2:
                 _ = struct.unpack("<B", stream.read(1))[0]
-
+            if version >= 3:
+                _ = struct.unpack("<B", stream.read(1))[0]
 
             code = struct.unpack("<B", stream.read(1))[0]
             self.dtype = DType.dtype_from_code(code)
